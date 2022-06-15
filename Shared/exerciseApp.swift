332e91300c8f368ab1b8cd2ac88dfbc0f5f5b353
Imar Rubio <imarcrubio@gmail.com>
2022-06-15 02:06:46 -0500

    project setup

---
+++ Shared/exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseApp.swift
+//  Shared
+//
+//  Created by Ella Rubio on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
